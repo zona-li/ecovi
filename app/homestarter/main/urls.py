ab705c05474b3c5f4a7e361e38fd608f8c88682a
ZonaClark <zona.clark56@gmail.com>
2017-08-09 11:24:04 -0400

    fix footer links

--- app/homestarter/main/urls.py
+++ app/homestarter/main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='homepage'),
 	url(r'^contact/$', views.contact, name='contact'),
+	url(r'^linkedin/$', views.linkedin, name='linkedin'),
+	url(r'^facebook/$', views.facebook, name='facebook'),
+	url(r'^instagram/$', views.instagram, name='instagram'),
 ]
--- app/homestarter/main/views.py
+++ app/homestarter/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
@@ -6,3 +6,12 @@ def index(request):
 
 def contact(request):
 	return render(request, 'main/basic.html', {'category':['For homeowners: ','For contractors: '], 'products':['remodeling','designing','energy consulting']})
+
+def linkedin(request):
+	return redirect('https://www.linkedin.com/company-beta/11119216/')
+
+def facebook(request):
+	return redirect('https://www.facebook.com/')
+
+def instagram(request):
+	return redirect('https://www.instagram.com/homelyfit/')
