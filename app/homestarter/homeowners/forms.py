bf0f2ea4cf0eb88674f94c5edb4ea229a4cae02b
ZonaClark <zona.clark56@gmail.com>
2017-08-15 09:00:22 -0400

    homeowner signup page working

--- app/homestarter/homeowners/forms.py
+++ app/homestarter/homeowners/forms.py
@@ -1,13 +1,19 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+
+from django.forms import ModelForm
+from .models import *
 
 
-class SignUpForm(UserCreationForm):
-    firstname = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    lastname = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    useremail = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+class SignUpForm(forms.ModelForm):
+    # firstname = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    # lastname = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    # useremail = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     class Meta:
-        model = User
-        fields = ('username', 'firstname', 'lastname', 'useremail', 'password',)
+        model = Homeowner
+        widgets = {
+        'password': forms.PasswordInput(),
+        }
+        fields = '__all__'
