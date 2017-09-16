fdd8beb2e69500148f21abe8fe95576aef795abe
ZonaClark <zona.clark56@gmail.com>
2017-09-16 19:02:21 -0400

    update model

--- app/homestarter/api/models.py
+++ app/homestarter/api/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Project(models.Model):
-	pass
+	project_name = models.CharField(max_length=255, blank=False, unique=True)
+	project_description = models.TextField()
+	date_modified = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return "{}".format(self.name)
--- app/homestarter/api/tests.py
+++ app/homestarter/api/tests.py
@@ -5,10 +5,10 @@ class ModelTestCase(TestCase):
 	
 	def setUp(self):
 		self.project_name = "cville solar"
-		self.project = Project(name = self.project_name)
+		self.project = Project(project_name = self.project_name)
 
 	def test_model_create_project(self):
 		old_count = Project.objects.count()
-        self.project.save()
-        new_count = Project.objects.count()
-        self.assertNotEqual(old_count, new_count)
+		self.project.save()
+		new_count = Project.objects.count()
+		self.assertNotEqual(old_count, new_count)
