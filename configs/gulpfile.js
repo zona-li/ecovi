ef4008c12641ec0c2c7d5d3d40b351a50f5c4a18
ZonaClark <zona.clark56@gmail.com>
2017-09-20 20:15:55 -0400

    gulp setup

--- configs/gulpfile.js
+++ configs/gulpfile.js
@@ -5,3 +5,9 @@ gulp.task('styles', ()=> {
 	.pipe(sass())
 	.pipe(gulp.dest('./css'));
 });
+
+gulp.task('watch', ()=> {
+	gulp.watch('./sass/**/*.scss', ['styles']);
+});
+
+gulp.task('default', ['styles']);
